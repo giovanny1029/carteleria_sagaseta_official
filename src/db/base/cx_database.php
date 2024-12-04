88acfde3fed9e85b5b95cb3c43c3d904a2733549
Aileen <aileenpadrontorres511@.com>
2024-12-04 11:36:21 +0000

    Added cx_database

--- src/db/base/cx_database.php
+++ src/db/base/cx_database.php
@@ -1,1 +1,26 @@
+<?php
 
+function enableErrorLog()
+{
+  error_reporting(E_ALL);
+  ini_set('display_errors', '1');
+  return true;
+}
+
+function createConnection()
+{
+  try {
+    $host = "localhost";
+    $db = "carteleria";
+    $user = "root";
+    $pass = "";
+    $dns = "mysql:host=$host;dbname=$db";
+    $conn = new PDO($dns, $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+  } catch (PDOException $ex) {
+    error_log("Error en la conexión a la base de datos: " .
+      $ex->getMessage());
+    return null;
+  }
+}
