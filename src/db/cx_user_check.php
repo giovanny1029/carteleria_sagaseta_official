d6d9320c99c69281d107028ded0c5dd166dde2da
Aileen Padrón Torres <79520153+PinkBlure@users.noreply.github.com>
2024-12-06 18:23:42 +0000

    Update cx_user_check.php

--- src/db/cx_user_check.php
+++ src/db/cx_user_check.php
@@ -4,7 +4,7 @@
     // Importacion de conexión a la base de datos
     require_once "base/cx_database.php";
 
-    // Función para comrpobar que los datos pasados por parámetro existen en la base de datos
+    // Función para comprobar que los datos pasados por parámetro existen en la base de datos
     function checkUser($cial, $pin){
         try {
         // Crear conexión
@@ -30,7 +30,7 @@
         }
     }
     
-    // Función para comrpobar que el usuario cuyo cial es pasado por parámetro haya participado anteriormente
+    // Función para comprobar que el usuario cuyo cial es pasado por parámetro haya participado anteriormente
     function userParticiped($cial){
         try {
         // Crear conexión
@@ -56,7 +56,7 @@
         }
     }
     
-    // Función para comrpobar que el usuario cuyo cial es pasado por parámetro haya votado anteriormente
+    // Función para comprobar que el usuario cuyo cial es pasado por parámetro haya votado anteriormente
     function userVoted($cial){
         try {
         // Crear conexión
