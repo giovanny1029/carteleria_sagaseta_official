c5a2de94ae4432cb11fa8f43209e6b5242e77a24
MJSDaw <mjsdaw@gmail.com>
2024-12-06 15:01:48 +0000

    Tratamiento de obtención de datos como función.

--- src/db/cx_date.php
+++ src/db/cx_date.php
@@ -1,17 +1,24 @@
 <?php
+    // Importacion de conexión a la base de datos
     require_once "base/cx_database.php";
 
+    // Crear conexión
     $conn = createConnection();
 
-    try {
-        $stmt = $conn->prepare("
-            SELECT fecha, descripcion
-            FROM fecha;
-        ");
-        $stmt->execute();
-    
-        $result = $stmt->fetchAll(PDO::FETCH_BOTH);
-    } catch (PDOException $e) {
-        echo "Error al realizar la consulta: " . $e->getMessage();
+    // Función para obtener las fechas y sus descripciones
+    function getDates(){
+        try {
+            // Query para obtener los datos
+            $stmt = $conn->prepare("
+                SELECT fecha, descripcion
+                FROM fecha;
+            ");
+            $stmt->execute();
+        
+            return $stmt->fetchAll(PDO::FETCH_BOTH);
+        } catch (PDOException $e) {
+            echo "Error al realizar la consulta: " . $e->getMessage();
+        }
     }
+
 ?>
