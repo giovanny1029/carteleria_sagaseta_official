25f470558450517e0bdd9994fa8c4157c328c6ad
MJSDaw <mjsdaw@gmail.com>
2024-12-06 13:28:53 +0000

    Realizado script para recoger las fechas y descripciones desde la base de datos.

--- src/db/cx_date.php
+++ src/db/cx_date.php
@@ -1,1 +1,17 @@
+<?php
+    require_once "base/cx_database.php";
 
+    $conn = createConnection();
+
+    try {
+        $stmt = $conn->prepare("
+            SELECT fecha, descripcion
+            FROM fecha;
+        ");
+        $stmt->execute();
+    
+        $result = $stmt->fetchAll(PDO::FETCH_BOTH);
+    } catch (PDOException $e) {
+        echo "Error al realizar la consulta: " . $e->getMessage();
+    }
+?>
