d83f967756b60dcd6276ae35f34a88f665254b03
MJSDaw <mjsdaw@gmail.com>
2024-12-06 15:44:59 +0000

    Realización de script para recoger los datos de autor y cartel + votos desde la base de datos.

--- src/db/cx_results.php
+++ src/db/cx_results.php
@@ -1,1 +1,27 @@
+<?php
+    // Importacion de conexión a la base de datos
+    require_once "base/cx_database.php";
 
+    // Crear conexión
+    $conn = createConnection();
+
+    // Función para obtener los carteles ganadores
+    function getResults($idCartel){
+        try {
+            // Query para obtener los datos
+            $stmt = $conn->prepare("
+                SELECT u.nombre, u.curso, c.titulo, c.descripcion, c.imagen, (SELECT DISTINCT COUNT(1) from votacion where id_cartel = c.id) votos 
+                FROM usuario u, cartel c, participacion p 
+                WHERE u.cial = p.id_usuario AND p.id_cartel = c.id
+                ORDER BY votos DESC
+                LIMIT 3;
+            ");
+            $stmt->execute();
+        
+            return $stmt->fetchAll(PDO::FETCH_BOTH);
+        } catch (PDOException $e) {
+            echo "Error al realizar la consulta: " . $e->getMessage();
+        }
+    }
+
+?>
